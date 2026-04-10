7c99f836d6293eb0a76a8643503d25948519ec6d
Chirag-200412 <chiragagrawal1500@gmail.com>
2026-04-10 14:27:39 +0530

    Updated

--- NewServer3.0/server.js
+++ NewServer3.0/server.js
@@ -6,10 +6,8 @@ const User = require("./models/userModels.js");
 
 const userRoutes = require("./routes/userRoutes");
 
-// Connect DB
 connectDB();
 
-// getUser();
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -19,5 +17,5 @@ app.get("/", (req, res) => {
 app.use("/users", userRoutes);
 
 app.listen(8080, () => {
-  console.log("Server is listening on port 8080");
+  console.log("Server is listening on http://localhost:8080/");
 });
